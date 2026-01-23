83f28b8b2792c1c59b2c6070324fbded8318506a
boneseva <eva.bones@lgm.fri.uni-lj.si>
2026-01-23 15:15:43 +0100

    fix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dash import Dash, html
 from werkzeug.middleware.profiler import ProfilerMiddleware
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from layout import create_layout
 import read_data
@@ -10,6 +11,9 @@ app.title = "VibroVis"
 server = app.server
 server.secret_key = 'vibrovis-secure-key-change-this-in-env'
 server.config['WTF_CSRF_ENABLED'] = False
+
+# Apply ProxyFix to handle HTTPS headers correctly
+server.wsgi_app = ProxyFix(server.wsgi_app, x_proto=1, x_host=1)
 
 app.index_string = '''
 <!DOCTYPE html>
