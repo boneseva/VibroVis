735b0593af1af9cae6da9667e27209521bf4d0d3
boneseva <eva.bones@lgm.fri.uni-lj.si>
2026-01-23 14:51:04 +0100

    changed dockerfile

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from callbacks import set_initial_data, register_callbacks
 
 app = Dash(__name__)
 app.title = "VibroVis"
+server = app.server
 
 app.index_string = '''
 <!DOCTYPE html>
@@ -30,27 +31,7 @@ app.index_string = '''
 </html>
 '''
 
-app.index_string = '''
-<!DOCTYPE html>
-<html>
-    <head>
-        {%metas%}
-        <title>{%title%}</title>
-        <link rel="icon" type="image/svg+xml" href="/assets/favicon.svg">
-        <link rel="shortcut icon" type="image/svg+xml" href="/assets/favicon.svg">
-        <link rel="apple-touch-icon" href="/assets/favicon.svg">
-        {%css%}
-    </head>
-    <body>
-        {%app_entry%}
-        <footer>
-            {%config%}
-            {%scripts%}
-            {%renderer%}
-        </footer>
-    </body>
-</html>
-'''
+
 
 initial_df = read_data.get_initial_data_for_layout()
 set_initial_data(initial_df)
@@ -60,16 +41,16 @@ app.layout = create_layout(initial_df)
 register_callbacks(app)
 
 if __name__ == "__main__":
-    # server = app.server
-    #
-    # PORT = 8050
-    # ADDRESS = "0.0.0.0"
-    #
-    # app.run(port=PORT, host=ADDRESS, debug=True, use_reloader=False, dev_tools_ui=True)
+    PORT = 8050
+    ADDRESS = "0.0.0.0"
 
-    app.server.wsgi_app = ProfilerMiddleware(
-        app.server.wsgi_app,
-        sort_by=['cumtime']
-    )
-
-    app.run(port=8050, host="0.0.0.0", debug=False, threaded=False)
+    display_port = PORT
+    if ADDRESS == "0.0.0.0":
+        display_host = "127.0.0.1"
+    else:
+        display_host = ADDRESS
+        
+    print(f"Dash is running on http://{display_host}:{display_port}/")
+    print("WARNING: This is a PRODUCTION configuration. Debug mode is OFF.")
+    
+    app.run(port=PORT, host=ADDRESS, debug=False, use_reloader=False, dev_tools_ui=False)
